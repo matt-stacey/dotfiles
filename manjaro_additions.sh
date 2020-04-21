2b06cf1197bbde75bf9d003b15622387d9064848
Matt Stacey @ FX505DU-manjaro <53570754+matt-stacey@users.noreply.github.com>
2020-04-21 09:19:45 -0500

    updates to manjaro

--- manjaro_additions.sh
+++ manjaro_additions.sh
@@ -12,8 +12,14 @@ pacman -S opera vim terminator sudo which iw wpa_supplicant dhcpcd iwd grub efib
 os-prober
 grub-mkconfig -o /boot/grub/grub.cfg
 
+# Get rid of old VI and simlink to VIM
+pacman -R vi
+cd /usr/bin
+ln -s vim vi
+cd -
+
 # Install some software
-pacman -S byobu sshfs openvpn firefox python python3 python-pip evince code htop powertop iotop tlp vlc redshift libreoffice-still wine play-on-linux steam lib32-nvidia-utils lib32-mesa ttf-liberation openra gcompris-qt
+pacman -S byobu sshfs openvpn firefox python python3 python-pip evince code htop powertop iotop tlp vlc redshift libreoffice-still wine playonlinux steam lib32-nvidia-utils lib32-mesa ttf-liberation openra gcompris-qt openscad xscreensaver
 tlp start
 systemctl enable tlp.service
 
@@ -21,6 +27,12 @@ systemctl enable tlp.service
 echo 'press enter to reboot or ctrl-C to exit'
 read
 shutdown -r now
+
+# Additional non-package software
+# Pycharm: https://www.jetbrains.com/pycharm/download/#section=linux$
+# APM: https://ardupilot.org/planner2/docs/installation-for-linux.html#installation-for-linux
+
+# see linux.notes for settings setup
 
 # Install drivers
 pacman -S nvidia
