1779fb5ef10293b8f9c05ff1905773c8a78349b1
Matt Stacey @ FX505DU-manjaro <53570754+matt-stacey@users.noreply.github.com>
2020-04-29 19:06:36 -0500

    finalizing grub

--- unpack.sh
+++ unpack.sh
@@ -1,26 +1,36 @@
 #!/bin/bash -v
 
 
-# copies vim files from this repo
-echo 'copying .vimrc'
-cp .vimrc ~/.vimrc
+## copies vim files from this repo
+#echo 'copying .vimrc'
+#cp .vimrc ~/.vimrc
+#
+#echo 'copying vim color schemes'
+#mkdir -p ~/.vim/colors
+#cp -r ./.vim/colors ~/.vim
+#
+#
+## copies bash files from this repo
+#echo 'copying .bashrc'
+#cp .bashrc ~/.bashrc
+#
+#echo 'copying bash aliases'
+#cp .bash_aliases ~/.bash_aliases
+#
+#
+## source files
+#
+#
+#
+## run scripts
+#
 
-echo 'copying vim color schemes'
-mkdir -p ~/.vim/colors
-cp -r ./.vim/colors ~/.vim
+# init flags
+GRUB=0
+LIGHTDM=0
+
+# consume args and set flags
 
 
-# copies bash files from this repo
-echo 'copying .bashrc'
-cp .bashrc ~/.bashrc
-
-echo 'copying bash aliases'
-cp .bash_aliases ~/.bash_aliases
-
-
-# source files
-
-
-
-# run scripts
+# unpack as requested
 
