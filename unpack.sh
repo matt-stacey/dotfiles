97ffb8e91e020fadf3013e4264a39af06d5521e8
Matt Stacey @ FX505DU-manjaro <53570754+matt-stacey@users.noreply.github.com>
2020-05-05 01:40:10 -0500

    unpack skeleton

--- unpack.sh
+++ unpack.sh
@@ -28,9 +28,80 @@
 # init flags
 GRUB=0
 LIGHTDM=0
+BASH=1
+VIM=1
+THEMES=1
 
 # consume args and set flags
+while (( $# )); do
 
+    case $1 in
+        --grub)
+            GRUB=1
+            ;;
+        --lightdm)
+            LIGHTDM=1
+            ;;
+        --no-bash)
+            BASH=0
+            ;;
+        --no-vim)
+            VIM=0
+            ;;
+        --no-themes)
+            THEMES=0
+            ;;
+        --print_next)
+            echo 'here it is: '$2
+            shift
+            ;;
+        --*)
+            echo 'bad option: '$1
+            ;;
+        -*)
+            echo 'not parsed: '$1
+            ;;
+    esac
+    shift
+done
 
 # unpack as requested
+if [ $GRUB ]
+then
+    echo 'unpacking GRUB theme'
 
+else
+    echo 'GRUB skipped!'
+fi
+
+if [ $LIGHTDM ]
+then
+    echo 'unpacking LightDM theme'
+
+else
+    echo 'LightDM skipped!'
+fi
+
+if [ $BASH ]
+then
+    echo 'unpacking bashrc and aliases'
+
+else
+    echo 'bashrc and aliases skipped!'
+fi
+
+if [ $VIM ]
+then
+    echo 'unpacking vim config and color schemes'
+    
+else
+    echo 'vim config skipped!'
+fi
+
+if [ $THEMES ]
+then
+    echo 'unpacking themes'
+
+else
+    echo 'themes skipped!'
+fi
